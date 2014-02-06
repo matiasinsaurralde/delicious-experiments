1e169eb753faedd6162e2401a681532761ff7720
Matias Insaurralde <matiasbaruchinsaurralde@gmail.com>
2014-02-06 03:41:01 -0300

    extending hash for not-returning an array after #sort_by

--- main.rb
+++ main.rb
@@ -1,7 +1,22 @@
 require 'nokogiri'
 require 'pp'
 
-hours, bookmarks = {}, Nokogiri::HTML( File.read( 'bookmarks.html' ) ).css('a').map do |bookmark|
+class Hash
+  def fill_empty_hours!( default_value = 0.0 )
+    23.times.to_a.each do |hour|
+      if !self[ hour ]
+        self.store( hour, default_value )
+      end
+    end
+  end
+  def sort_by_hour()
+    _hash = {}
+    self.sort_by { |k,v| k }.each { |i| _hash.store( i[0], i[1] ) }
+    return _hash
+  end
+end
+
+months, bookmarks = {}, Nokogiri::HTML( File.read( 'bookmarks.html' ) ).css('a').map do |bookmark|
     if bookmark.attr('tags')
       { :timestamp => Time.at( bookmark.attr('add_date').to_i ), :url => bookmark.attr('href'),
         :tags => bookmark.attr('tags').split(','), :title => bookmark.inner_text()
@@ -11,11 +26,25 @@ end
 
 bookmarks.each do |bookmark|
   if bookmark
-    hour = bookmark[:timestamp].strftime('%H').to_i
-    hours[ hour ] = 0 if !hours[ hour ]
-    hours[ hour ] += 1
+    month = bookmark[ :timestamp ].strftime( '%m/%Y' )
+    months.store( month, {} ) if !months[ month ]
+
+    hour = bookmark[ :timestamp ].strftime( '%H' ).to_i
+    months[ month ].store( hour, 0 ) if !months[ month ][ hour ]
+    months[ month ][ hour ] += 1
   end
 end
 
-hours.each { |hour, n| hours[hour] = ( n/bookmarks.size.to_f*100.0 ).round(2) }
-pp hours.sort_by { |hour, n| hour }
+months.each do |month, hours|
+
+  month_total, _hours = hours.values.inject( :+ ), {}
+
+  hours.each { |hour, n| _hours[ hour ] = ( n/month_total.to_f*100.0 ).round(1) }
+
+  _hours.fill_empty_hours!
+
+  puts "-> Month: #{month}, total: #{month_total}"
+
+  pp _hours.sort_by_hour
+
+end
